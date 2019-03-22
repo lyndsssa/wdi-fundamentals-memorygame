e1e9be8d6e352aaa5c1a5cb0b3ac973d6c85fda1
lyndsey ajster <lyndsssa@yahoo.com>
2019-03-22 15:13:08 -0700

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,7 +1,22 @@
 main.js
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
+var cards=['queen', 'queen', 'king', 'king'];
+cards.length;
+var cardsInPlay=[cardOne, cardTwo,];
+var cardOne= cards[0];
+cards.push("cardOne");
+console.log(cards.[0]); // will be "queen"
+var cardTwo= cards[1];
+cards.push("cardTwo");
+console.log(cards[2]); //will show "king"
+if (x=2){
+	console.log ('cardsInPlay');
+}
+if (x===2){
+	(cards.log('cardsInPlay'));
+}
+if (card < 2) {
+	greeting= "Sorry, try again."
+}
+
